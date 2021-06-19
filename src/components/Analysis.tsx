d69319e1aba9af94da175cc6d0583f2558ef27a1
ken imoto <k096906261.49@gmail.com>
2021-06-19 23:04:22 +0900

    波形の描画

---
+++ src/components/Analysis.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+
+
+type MyState = {
+  isAudio: boolean
+  volume: number
+  recordingFlg: boolean
+};
+class Analysis extends React.Component<{}, MyState> {
+  private localAudio?: HTMLVideoElement | null
+  private localMediaStream: MediaStream | null
+  private audioContext: AudioContext | null
+  private audioData: any[]
+  private audioAnalyser: any
+  private canvas?: HTMLCanvasElement | null
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isAudio: false,
+      volume: 0,
+      recordingFlg: false,
+    };
+    this.localAudio = null
+    this.localMediaStream = null
+    this.audioContext = new AudioContext();
+    this.audioData = []
+    this.audioAnalyser = null
+  }
+
+  endRecording = () => {
+    this.setState({recordingFlg: false})
+  }
+
+  startRecording = () => {
+    this.setState({recordingFlg: true})
+    if (this.localAudio && this.localAudio.srcObject) {
+      this.localAudio.srcObject = null
+      this.setState({isAudio: false})
+    }
+    navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then((stream: MediaStream) => {
+      this.localMediaStream = stream;
+      const bufferSize = 1024
+      if (this.audioContext) {
+        let scriptProcessor = this.audioContext.createScriptProcessor(bufferSize, 1, 1);
+        let mediastreamsource = this.audioContext.createMediaStreamSource(stream);
+        mediastreamsource.connect(scriptProcessor);
+        scriptProcessor.onaudioprocess = this.onAudioProcess;
+        scriptProcessor.connect(this.audioContext.destination);
+
+        // 音声解析関連
+        this.audioAnalyser = this.audioContext.createAnalyser();
+        this.audioAnalyser.fftSize = 2048;
+        const frequencyData = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+        const timeDomainData = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+        mediastreamsource.connect(this.audioAnalyser); 
+      }
+    })
+  }
+
+  onAudioProcess = (e: any) => {
+    if (!this.state.recordingFlg) return;
+    const bufferSize = 1024
+    // 音声のバッファを作成
+    var input = e.inputBuffer.getChannelData(0);
+    var bufferData = new Float32Array(bufferSize);
+    for (var i = 0; i < bufferSize; i++) {
+        bufferData[i] = input[i];
+    }
+    this.audioData.push(bufferData);
+    // 波形を解析
+    this.analyseVoice();
+  }
+
+  analyseVoice = () => {
+    if (!this.audioContext) return
+    let fsDivN = this.audioContext.sampleRate / this.audioAnalyser.fftSize;
+    let spectrums = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+    this.audioAnalyser.getByteFrequencyData(spectrums);
+    if (!this.canvas) return
+    const canvasContext = this.canvas.getContext('2d');
+    if (!canvasContext) return
+    canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    canvasContext.beginPath();
+    for (let i = 0, len = spectrums.length; i < len; i++) {
+      //canvasにおさまるように線を描画
+      let x = (i / len) * this.canvas.width;
+      let y = (1 - (spectrums[i] / 255)) * this.canvas.height;
+      if (i === 0) {
+        canvasContext.moveTo(x, y);
+      } else {
+        canvasContext.lineTo(x, y);
+      }
+      let f = Math.floor(i * fsDivN);  // index -> frequency;
+
+      // 500 Hz単位にy軸の線とラベル出力
+      if ((f % 500) === 0) {
+        let text = (f < 1000) ? (f + ' Hz') : ((f / 1000) + ' kHz');
+        // Draw grid (X)
+        canvasContext.fillRect(x, 0, 1, this.canvas.height);
+        // Draw text (X)
+        canvasContext.fillText(text, x, this.canvas.height);
+      }
+    }
+    canvasContext.stroke();
+    // x軸の線とラベル出力
+    let textYs = ['1.00', '0.50', '0.00'];
+    for (let i = 0, len = textYs.length; i < len; i++) {
+      let text = textYs[i];
+      let gy = (1 - parseFloat(text)) * this.canvas.height;
+      // Draw grid (Y)
+      canvasContext.fillRect(0, gy, this.canvas.width, 1);
+      // Draw text (Y)
+      canvasContext.fillText(text, 0, gy);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>音声解析</h2>
+        <div>
+          <button onClick={() => {
+            this.startRecording()
+          }}>解析開始</button>
+          <button onClick={() => {
+            this.endRecording()
+          }}>解析終了</button>
+        </div>
+        <canvas style={{ width: '400px'}} ref={(canvas: HTMLCanvasElement) => (this.canvas = canvas)} />
+        <video
+          ref={(video: HTMLVideoElement) => (this.localAudio = video)} />
+      </div>
+    );
+  }
+}
+
+// クラスをexport
+export default Analysis;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,59 +1,102 @@
 import React from 'react';
+import Analysis from './Analysis'
 
 
 type MyState = {
+  isAudio: boolean
+  volume: number
 };
 class Main extends React.Component<{}, MyState> {
-  private localAudio?: HTMLVideoElement | null;
+  private localAudio?: HTMLVideoElement | null
+  private sound: AnalyserNode | null
+  private micAudio: any
   constructor(props: any) {
     super(props);
     this.state = {
+      isAudio: false,
+      volume: 0,
     };
     this.localAudio = null
+    this.sound = null
+    this.micAudio = null
   }
 
-
-  componentWillMount = () => {
-    
-  }
-
-  transformAudio = (event: 'start' | 'stop') => {
+  setAudio = (event: 'start' | 'stop') => {
     if (event === 'start') {
       if (this.localAudio && this.localAudio.srcObject) {
         this.localAudio.srcObject = null
+        this.setState({isAudio: false})
       }
-      navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then((localStream: MediaStream) => {
+      this.micAudio = navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then((localStream: MediaStream) => {
         console.warn('localStream', localStream)
         if (this.localAudio) {
+          this.setState({isAudio: true})
           this.localAudio.srcObject = localStream
           this.localAudio.play()
+          this.setAudioVolume(localStream)
+          setInterval(() => {
+            if (this.sound) {
+              const volume = this.getAudioVolume(this.sound)
+              this.setState({volume})
+            }
+          }, 1000)
         }
       })
     } else {
       if (this.localAudio && this.localAudio.srcObject) {
+        // if(this.micAudio)
+        //   this.micAudio.getTracks().forEach(function(track: any) {
+        //     track.stop();
+        //   });
         this.localAudio.srcObject = null
-      }
+        this.setState({isAudio: false})
+      };
     }
-    
   }
+
+  setAudioVolume = (mediaStream: MediaStream) => {
+    const audioContext = new AudioContext();
+    this.sound = audioContext.createAnalyser();
+    this.sound.fftSize = 128;
+    const source = audioContext.createMediaStreamSource(mediaStream!);
+    source.connect(this.sound);
+  }
+
+  getAudioVolume = (sound: AnalyserNode ) => {
+    const bit8 = new Uint8Array(sound.frequencyBinCount);
+    sound.getByteFrequencyData(bit8);
+    return (
+      bit8.reduce((previous, current) => {
+        return previous + current;
+      }) / sound.frequencyBinCount
+    )
+  }
+
+  // transformAudio = () => {
+
+  // }
 
   render() {
     return (
       <>
         <div>
           <h1>ボイスチェンジテスト用</h1>
-          <button onClick={() => {
-            this.transformAudio('start')
-          }}>変換開始</button>
-          <button onClick={() => {
-            this.transformAudio('stop')
-          }}>音声の取得を停止する</button>
-          {this.localAudio?.srcObject ? (
-            <p>再生中...</p>
-          ) : (<>音声は取得していません</>) }
-          <video
+          <div>
+            <button onClick={() => {
+              this.setAudio('start')
+            }}>変換開始</button>
+            <button onClick={() => {
+              this.setAudio('stop')
+            }}>音声の取得を停止する</button>
+            {this.state.isAudio ? (
+              <p>再生中...</p>
+            ) : (<p>音声は取得していません</p>)}
+          </div>
+          {this.state.volume && (<p>音量：{this.state.volume}</p>)}
+          <video style={{height: '0px'}}
               ref={(video: HTMLVideoElement) => (this.localAudio = video)} />
         </div>
+        <Analysis />
       </>
     );
   }
